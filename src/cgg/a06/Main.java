117a8d812891f489ea90bf3c893b72e9dfc8e63e
Lightbirdv <vicolino@gmx.de>
2021-05-30 20:03:44 +0200

    a06 added

--- src/cgg/a06/Main.java
+++ src/cgg/a06/Main.java
@@ -31,17 +31,45 @@ public class Main {
         shapes.add(globe7);
         //shapes.add(globe6);
         Group group = new Group(shapes);
+
+        ArrayList<Shape> shapes2 = new ArrayList<Shape>();
+        Shape background2 = new Background(multiply(2,color(0,1,1)), new BackgroundMaterial(white));
+        Shape ground2 = new Plane(point(0.0, -0.5, 0.0), direction(0, 1, 0), color(0.2,0.8,0), Double.POSITIVE_INFINITY, new DiffuseMaterial(gray));
+        Shape sphere1 = new Sphere(point(-2.0, -0.25, -2.5), 0.3, red, new Metal(0));
+        Shape sphere2 = new Sphere(point(0.0, 0.75, -2.5), 0.2, green, new DiffuseMaterial(green));
+        Shape sphere3 = new Sphere(point(1.0, 0.25, -2.5), 0.5, blue, new DiffuseMaterial(blue));
+        Shape sphere4 = new Sphere(point(1.2, 0.25, -1.5), 0.1, orange, new DiffuseMaterial(orange));
+        Shape sphere5 = new Sphere(point(1.0, 1, -2.5), 0.3, cyan, new DiffuseMaterial(cyan));
+        Shape sphere6 = new Sphere(point(0.1, 0.3, -1.5), 0.4, green, new Glass());
+        Shape sphere7 = new Sphere(point(-1.0, 1, -2.5), 0.2, red, new Metal(0.4, color(0.2,0,0)));
+        shapes2.add(background2);
+        shapes2.add(ground2);
+        shapes2.add(sphere1);
+        shapes2.add(sphere2);
+        shapes2.add(sphere3);
+        shapes2.add(sphere4);
+        shapes2.add(sphere5);
+        shapes2.add(sphere6);
+        shapes2.add(sphere7);
+        //shapes.add(globe6);
+        Group group2 = new Group(shapes2);
+
         Image image = new Image(width, height);
         Camera cam = new Camera(Math.PI/2, width, height);
-
+        Image image2 = new Image(width, height);
+        Camera cam2 = new Camera(Math.PI/2, width, height);
        
         Raytracing raytraycing = new Raytracing(group, cam, 6);
+        Raytracing raytraycing2 = new Raytracing(group2, cam2, 6);
         
         image.supersample(raytraycing, 10);
-        
+        image2.supersample(raytraycing2, 10);
         final String filename = "doc/a06-mirrors-glass-1.png";
         image.write(filename);
         System.out.println("Wrote image basic: " + filename);
+        final String filename2 = "doc/a06-mirrors-glass-2.png";
+        image2.write(filename2);
+        System.out.println("Wrote image basic: " + filename2);
         //test();
     }
 
